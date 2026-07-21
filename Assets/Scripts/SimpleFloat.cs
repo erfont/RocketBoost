8b6a5afc4a343a32209eba9256cae220e552abe8
Jose Maria Font Fernandez <jose.font@mau.se>
2026-07-21 12:26:30 +0200

    Scene improv

--- Assets/Scripts/SimpleFloat.cs
+++ Assets/Scripts/SimpleFloat.cs
@@ -5,6 +5,8 @@ public class SimpleFloat : MonoBehaviour
     [SerializeField] private float amplitude = 0.5f; // how far it moves up/down
     [SerializeField] private float frequency = 1f;    // how fast it moves
 
+    [SerializeField] AnimationCurve frequencyCurve;
+
     private Vector3 startPos;
 
     void Start()
@@ -14,7 +16,12 @@ public class SimpleFloat : MonoBehaviour
 
     void Update()
     {
-        float newY = startPos.y + Mathf.Sin(Time.time * frequency) * amplitude;
+        // // Calculating newY via Sin function
+        // float newY = startPos.y + Mathf.Sin(Time.time * frequency) * amplitude;
+        
+        // Calculating newY via AnimationCurve
+        float newY = startPos.y + frequencyCurve.Evaluate(Time.time * frequency) * amplitude;
+
         transform.position = new Vector3(startPos.x, newY, startPos.z);
     }
 }
